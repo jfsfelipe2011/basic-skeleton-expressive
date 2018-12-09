6b9934c858fbbe15cdfc32778d0fa2ec1fb0ed63
José Felipe da Silva <jfsfelipe2011@gmail.com>
2018-12-09 17:38:32 -0200

    Ajuste para o uso de envs nas configurações

--- startapp.sh
+++ startapp.sh
@@ -5,5 +5,8 @@ export UID=$UID;
 echo "Permissões para configs"
 chmod 777 -R config/nginx
 
+echo "Gerando arquivo de env"
+cp .env.example .env
+
 echo "Subindo container com docker-compose";
 docker-compose up -d
